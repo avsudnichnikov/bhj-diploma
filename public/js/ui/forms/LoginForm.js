849fdfde164ac05245dae4c84cac4ba43f3f9787
avsudnichnikov <avsudnichnikov@gmail.com>
2021-01-07 23:07:43 +0500

    add auth forms

--- public/js/ui/forms/LoginForm.js
+++ public/js/ui/forms/LoginForm.js
@@ -1,16 +1,11 @@
-/**
- * Класс LoginForm управляет формой
- * входа в портал
- * Наследуется от AsyncForm
- * */
-class LoginForm {
-  /**
-   * Производит авторизацию с помощью User.login
-   * После успешной авторизации, сбрасывает форму,
-   * устанавливает состояние App.setState( 'user-logged' ) и
-   * закрывает окно, в котором находится форма
-   * */
+class LoginForm extends AsyncForm {
   onSubmit( options ) {
-
+    User.login (options.data, (err, response) => {
+      if (response && response.success) {
+        this.element.reset();
+        App.setState('user-logged');
+        App.getModal('login').close();
+      }
+    });
   }
 }
--- public/js/ui/forms/RegisterForm.js
+++ public/js/ui/forms/RegisterForm.js
@@ -1,15 +1,11 @@
-/**
- * Класс RegisterForm управляет формой
- * регистрации
- * Наследуется от AsyncForm
- * */
-class RegisterForm {
-  /**
-   * Производит регистрацию с помощью User.register
-   * После успешной регистрации устанавливает
-   * состояние App.setState( 'user-logged' )
-   * и закрывает окно, в котором находится форма
-   * */
+class RegisterForm extends AsyncForm {
   onSubmit( options ) {
+    User.register(options.data, (err, response) => {
+      if (response && response.success) {
+        this.element.reset();
+        App.setState( 'user-logged' );
+        App.getModal('register').close();
+      }
+    });
   }
 }
